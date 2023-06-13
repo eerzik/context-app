484a448de36db39e6b63a7049fe12a4f441a4ab9
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 14:59:14 +0300

    Birden çok useContext kullanımı

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,31 @@
 
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
 //Fonksiyonel component
-export default function Navbar() {
+const Navbar = () => {
+
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    const { isLightTheme, dark, light } =useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+
     return (
-        <AuthContext.Consumer>
-            {(authContext) => (
-                <ThemeContext.Consumer>{(context) => {
-                    const {isAuthenticated,toggleAuth} =authContext;
-                    const { isLightTheme, dark, light } = context;
-                    const theme = isLightTheme ? light : dark;
+        <nav style={{ background: theme.ui, color: theme.syntax }} >
+            <h1>AOS App</h1>
+            <div onClick={toggleAuth} >
+                {isAuthenticated ? "Çıkış Yap" : "Giriş Yap"}
+            </div>
+            <ul>
+                <li>Anasayfa</li>
+                <li>Hakkımızda</li>
+                <li>İletişim</li>
+            </ul>
+        </nav>
+    )
+}
 
-                    return (
-                        <nav style={{ background: theme.ui, color: theme.syntax }} >
-                            <h1>AOS App</h1>
-                            <div onClick={toggleAuth} >
-                                {isAuthenticated?"Çıkış Yap":"Giriş Yap"}
-                            </div>
-                            <ul>
-                                <li>Anasayfa</li>
-                                <li>Hakkımızda</li>
-                                <li>İletişim</li>
-                            </ul>
-                        </nav>
-                    )
-                }}
-                </ThemeContext.Consumer>
-            )
-            }
-        </AuthContext.Consumer>
-
-    );
-};
+export default Navbar
 
 
 
