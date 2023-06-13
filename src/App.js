ed6b620cd672d9a87555e0152e21a8fd7a3c9115
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 14:44:49 +0300

    Birden Fazla Context Kullanımı

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
 import ThemeContextProvider from "./contexts/ThemeContext";
-
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
-      <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
-      </ThemeContextProvider>
-
+      <AuthContextProvider>
+        <ThemeContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </ThemeContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
