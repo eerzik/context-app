2fa3fae9bcc06d0cbeffc6189baec24382697d0c
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 14:05:50 +0300

    Context ve Provider Oluşturma

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import BookList from "./Components/BookList";
 import Navbar from "./Components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
 
     </div>
   );
