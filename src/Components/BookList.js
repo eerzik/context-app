9bf29df38b35e87abbd118f79b450f197b4fb2a6
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 14:16:45 +0300

    Context Erişimi

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
-
+import { ThemeContext } from '../contexts/ThemeContext';
 class BookList extends Component {
+
+    static contextType = ThemeContext;
+
     render() {
+        const { isLightTheme, dark, light } = this.context;
+        const theme = isLightTheme ? light : dark;
+
         return (
-            <div className='book-list' >
+            
+            <div className='book-list' style={{background:theme.bg,color:theme.syntax}} >
                 <ul>
-                    <li>Var Mısın</li>
-                    <li>Hayvan Çifliği</li>
-                    <li>Bir Ömür Nasıl Yaşanır</li>
+                    <li style={{background:theme.ui}} >Var Mısın</li>
+                    <li style={{background:theme.ui}}>Hayvan Çifliği</li>
+                    <li style={{background:theme.ui}}>Bir Ömür Nasıl Yaşanır</li>
                 </ul>
             </div>
         );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,20 @@
 
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+
+    static contextType = ThemeContext;
+
     render() {
+
+        //  console.log(this.context);
+
+        const { isLightTheme, dark, light } = this.context;
+        const theme = isLightTheme ? light : dark;
+
         return (
-            <nav>
+            <nav style={{background:theme.ui,color:theme.syntax}} >
                 <h1>AOS App</h1>
                 <ul>
                     <li>Anasayfa</li>
